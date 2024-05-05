00c677c3f8f4ef589b8b1035c9d0a583a7c9f4a4
Haidara <haidaraibrahim963938@gmail.com>
2024-05-06 01:18:17 +0300

    modified tests.py

--- base/tests.py
+++ base/tests.py
@@ -7,83 +7,84 @@ DOMAIN = "http://127.0.0.1:8000/"
 # DOMAIN = "https://haidaraib.pythonanywhere.com"
 
 
-# class AddUserTestCase(TestCase):
-#     def test_add_user(self):
-#         requests.post(
-#             url=f"{DOMAIN}/addUser/",
-#             data={
-#                 "email": "test@gmail.com",
-#                 "password": "12345",
-#                 "username": "test",
-#             },
-#         )
+class AddUserTestCase(TestCase):
+    def test_add_user(self):
+        requests.post(
+            url=f"{DOMAIN}/addUser/",
+            data={
+                "email": "test@gmail.com",
+                "password": "12345",
+                "username": "test",
+            },
+        )
 
 
-# class LoginTestCase(TestCase):
-#     def test_login(self):
-#         r = requests.post(
-#             url=f"{DOMAIN}/login/",
-#             data={
-#                 "email": "test@gmail.com",
-#                 "password": "12345",
-#             },
-#         )
-#         print(r.text)
+class LoginTestCase(TestCase):
+    def test_login(self):
+        r = requests.post(
+            url=f"{DOMAIN}/login/",
+            data={
+                "email": "test@gmail.com",
+                "password": "12345",
+            },
+        )
 
 
 class AddBillTestCase(TestCase):
-    #     def test_add_el_bill(self):
-    #         requests.post(
-    #             url=f"{DOMAIN}/addBill/el/",
-    #             data={
-    #                 "payment_amount": 10,
-    #                 "commission_amount": 10,
-    #                 "date": "2010-10-10",
-    #                 "time": "11:11",
-    #                 "provider": "SyriatelSEP",
-    #                 "operation_number": "10",
-    #                 "gov": "A",
-    #                 "billing_number": "10",
-    #                 "invoice_number": "10",
-    #                 "subscription_number": "10",
-    #                 "user": "1",
-    #             },
-    #         )
+    def test_add_el_bill(self):
+        requests.post(
+            url=f"{DOMAIN}/addBill/el/",
+            data={
+                "payment_amount": 10,
+                "commission_amount": 10,
+                "date": "2010-10-10",
+                "time": "11:11",
+                "provider": "SyriatelSEP",
+                "operation_number": "10",
+                "gov": "A",
+                "billing_number": "10",
+                "invoice_number": "10",
+                "subscription_number": "10",
+                "user": "1",
+            },
+        )
 
-    #     def test_add_wa_bill(self):
-    #         requests.post(
-    #             url=f"{DOMAIN}/addBill/wa/",
-    #             data={
-    #                 "payment_amount": 10,
-    #                 "commission_amount": 10,
-    #                 "date": "2010-10-10",
-    #                 "time": "11:11",
-    #                 "provider": "SyriatelSEP",
-    #                 "operation_number": "10",
-    #                 "gov": "A",
-    #                 "receipt_number": "10",
-    #                 "barcode_number": "10",
-    #                 "counter_number": "10",
-    #                 "user": "1",
-    #             },
-    #         )
+    def test_add_wa_bill(self):
+        requests.post(
+            url=f"{DOMAIN}/addBill/wa/",
+            data={
+                "payment_amount": 10,
+                "commission_amount": 10,
+                "date": "2010-10-10",
+                "time": "11:11",
+                "provider": "SyriatelSEP",
+                "operation_number": "10",
+                "gov": "A",
+                "receipt_number": "10",
+                "barcode_number": "10",
+                "counter_number": "10",
+                "user": "1",
+            },
+        )
 
-    #     def test_add_tel_bill(self):
-    #         requests.post(
-    #             url=f"{DOMAIN}/addBill/tel/",
-    #             data={
-    #                 "payment_amount": 10,
-    #                 "commission_amount": 10,
-    #                 "date": "2010-10-10",
-    #                 "time": "11:11",
-    #                 "provider": "SyriatelSEP",
-    #                 "operation_number": "10",
-    #                 "phone_number_email": "10",
-    #                 "invoice_number": "10",
-    #                 "user": "1",
-    #             },
-    #         )
+    def test_add_tel_bill(self):
+        requests.post(
+            url=f"{DOMAIN}/addBill/tel/",
+            data={
+                "payment_amount": 10,
+                "commission_amount": 10,
+                "date": "2010-10-10",
+                "time": "11:11",
+                "provider": "SyriatelSEP",
+                "operation_number": "10",
+                "phone_number_email": "10",
+                "invoice_number": "10",
+                "user": "1",
+            },
+        )
 
+
+class AddBillsTestCase(TestCase):
     def test_add_tel_bills(self):
         r = requests.post(
             url=f"{DOMAIN}/addBills/tel/",
@@ -123,33 +124,124 @@ class AddBillTestCase(TestCase):
                 },
             ],
         )
-        print(r.text)
+
+    def test_add_wa_bills(self):
+        r = requests.post(
+            url=f"{DOMAIN}/addBills/wa/",
+            json=[
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "receipt_number": "10",
+                    "barcode_number": "10",
+                    "counter_number": "10",
+                    "user": "1",
+                },
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "receipt_number": "10",
+                    "barcode_number": "10",
+                    "counter_number": "10",
+                    "user": "1",
+                },
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "receipt_number": "10",
+                    "barcode_number": "10",
+                    "counter_number": "10",
+                    "user": "1",
+                },
+            ],
+        )
+
+    def test_add_el_bills(self):
+        r = requests.post(
+            url=f"{DOMAIN}/addBills/el/",
+            json=[
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "billing_number": "10",
+                    "invoice_number": "10",
+                    "subscription_number": "10",
+                    "user": "1",
+                },
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "billing_number": "10",
+                    "invoice_number": "10",
+                    "subscription_number": "10",
+                    "user": "1",
+                },
+                {
+                    "payment_amount": 10,
+                    "commission_amount": 10,
+                    "date": "2010-10-10",
+                    "time": "11:11",
+                    "provider": "SyriatelSEP",
+                    "operation_number": "10",
+                    "gov": "A",
+                    "billing_number": "10",
+                    "invoice_number": "10",
+                    "subscription_number": "10",
+                    "user": "1",
+                },
+            ],
+        )
 
 
-# class GetBillsTestCase(TestCase):
-#     def test_get_el_bills(self):
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/el/19/",
-#         )
+class GetBillsTestCase(TestCase):
+    def test_get_el_bills(self):
+        requests.get(
+            url=f"{DOMAIN}/getBills/el/19/",
+        )
 
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/el/1/",
-#         )
+        requests.get(
+            url=f"{DOMAIN}/getBills/el/1/",
+        )
 
-#     def test_get_wa_bills(self):
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/wa/19/",
-#         )
+    def test_get_wa_bills(self):
+        requests.get(
+            url=f"{DOMAIN}/getBills/wa/19/",
+        )
 
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/wa/1/",
-#         )
+        requests.get(
+            url=f"{DOMAIN}/getBills/wa/1/",
+        )
 
-#     def test_get_tel_bills(self):
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/tel/19/",
-#         )
+    def test_get_tel_bills(self):
+        requests.get(
+            url=f"{DOMAIN}/getBills/tel/19/",
+        )
 
-#         requests.get(
-#             url=f"{DOMAIN}/getBills/tel/1/",
-#         )
+        requests.get(
+            url=f"{DOMAIN}/getBills/tel/1/",
+        )
